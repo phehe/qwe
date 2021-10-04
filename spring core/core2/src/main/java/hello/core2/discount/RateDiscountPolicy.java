3db303e71680b1cc551761fb443348d51fea0d29
phehe <haeunoo@nate.com>
2021-10-04 21:06:59 +0900

    DiscountPolicy [Rate]

---
+++ spring core/core2/src/main/java/hello/core2/discount/RateDiscountPolicy.java
@@ -0,0 +1,19 @@
+package hello.core2.discount;
+
+import hello.core2.member.Grade;
+import hello.core2.member.Member;
+
+public class RateDiscountPolicy implements DiscountPolicy {
+
+    private int discountPercent = 10;
+
+
+    @Override
+    public int discount(Member member, int price) {
+        if (member.getGrade() == Grade.VIP) {
+            return price * discountPercent / 100;
+        } else {
+            return 0;
+        }
+    }
+}
